b507e366b2c19327b5f323663228ede7a3221e1a
Gabe-design <gabrielaross8@gmail.com>
2024-09-24 19:31:24 -0700

    new changes

---
+++ demo.js
@@ -0,0 +1,12 @@
+ console.log('42' == 42);
+
+ let x = 7;
+x++;
+x - 2;
+x *= 3;
+console.log(x);
+
+let num = 31;
+let isPositive = num > 0;
+let isEven = num % 2 === 0;
+console.log(isPositive && isEven);
